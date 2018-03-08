cc45250e28787fceb593b7ca36c2268425ae63d1
Kevin Huang <yellow.khaos@gmail.com>
2018-03-08 01:02:05 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/display_puppy' do
+  post '/new' do
     @new_pup = Puppy.new(params[:name],params[:breed],params[:age])
     erb :display_puppy
   end
