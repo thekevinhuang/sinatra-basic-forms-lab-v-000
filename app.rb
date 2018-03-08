f6ac17a1baebcbe908aac4ba03e41ddc998cf7f9
Kevin Huang <yellow.khaos@gmail.com>
2018-03-08 00:14:25 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require_relative 'config/environment'
+require_relative 'models/puppy'
 
 class App < Sinatra::Base
+  get '/' do
+    erb :index
+  end
 
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/display_puppy' do
+    erb display_puppy.erb
+  end
 end
